572ee6451a1b649f0b895df4f729ce0c8da42dea
Alex <altran@csu.fullerton.edu>
2021-04-29 22:40:11 -0700

    starting and messing with python curses

--- src/main.py
+++ src/main.py
@@ -1,9 +1,29 @@
-from utility import *
+from __init__ import *
+from enum import Enum, auto
+import time
+import curses
 
-print("something happened to main!")
+class State(Enum):
+	LOGIN = auto()
+
+def main():
+	screen = curses.initscr()
+
+	while True:
+		key = screen.getkey()
+		screen.clear()
+		screen.addstr(0, 0, banner)
+		screen.addstr(35, 10, key)
+		screen.refresh()
+
+		if key == 'KEY_Z':
+			break
+
+		curses.napms(100)
+	
+	curses.endwin()
+
+	print("Window ended.")
+
 if __name__ == "__main__":
-	clearTerm()
-	print(banner)
-	print(promptLogin)
-else:
-	print("i'm a library")
+	main()
